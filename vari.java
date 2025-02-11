0f6c45547580ea70146f88c42953831912e2e571
Gourav Sharma <gouravsharma2806@gmail.com>
2025-02-11 21:43:43 +0530

    variable

---
+++ vari.java
@@ -0,0 +1,34 @@
+public class vari {
+    public static void main() {
+        String name = "Gourav";
+        int age = 22;
+        String fullname = "Gourav Sharma";
+        String friend = name;
+
+        // data types
+        /*
+         * primitive types
+         * Byte - 1 [-128 to 127]
+         * short - 2
+         * int - 4
+         * long - 8
+         * float - 4
+         * double - 8
+         * char - 2 [ a,b,c,d]
+         * boolean - 1 [ true / false]
+         * 
+         * 
+         */
+
+        byte age1 = 30;
+        int phone = 6262566;
+        long phone1 = 123456L;
+        float pi = 3.14F;
+        char letter = 'a';
+        boolean isAdult = true;
+
+        // non primitive types refence type
+        String meraname = new String("apu");
+        System.out.println(meraname.length());
+    }
+}
